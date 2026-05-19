3162bc7321ff630048d4f58d41741b555067d307
Matheus Ribeiro Alves <matheusr21a@gmail.com>
2026-05-19 12:49:38 -0300

    feat:create validation

--- schemas/task.py
+++ schemas/task.py
@@ -1,14 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import Optional
 
 class taskCreate(BaseModel):
-    title: str
-    description:Optional[str] = None
+    title: str = Field(min_length=3, max_length=100)
+    description:Optional[str] = Field(None, max_length=500)
     
 class taskUpdate(BaseModel):
-    title: Optional[str] = None
-    description: Optional[str] = None
+    title: Optional[str] = Field(None, min_length=3, max_length=100)
+    description: Optional[str] = Field(None, max_length=500)
     completed: Optional[bool] = None
     
 class taskResponse(BaseModel):
